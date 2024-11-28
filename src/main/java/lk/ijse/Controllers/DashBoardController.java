58ce811fed33900b404697a58d82273d824c0433
pasindi <pasidingamage@gmail.com>
2024-11-28 20:48:46 +0530

    Update DashBoardController

--- src/main/java/lk/ijse/Controllers/DashBoardController.java
+++ src/main/java/lk/ijse/Controllers/DashBoardController.java
@@ -12,7 +12,11 @@ import lk.ijse.DAO.DAOFactory;
 import lk.ijse.DAO.Impl.LoginDAO;
 import lk.ijse.Entity.Login;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class DashBoardController {
 
@@ -30,9 +34,34 @@ public class DashBoardController {
     @FXML
     private Button btnUsers;
 
+    public Label time;
+
+    public  Label show_time;
+    public Label show_date;
+
     LoginDAO loginDAO = (LoginDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DaoType.Login);
     public void initialize() throws SQLException, ClassNotFoundException {
         lastLoginID();
+        setGreetings();
+        LocalDate();
+        LocalTime();
+    }
+
+    private void LocalDate() {
+        Date date = Date.valueOf(LocalDate.now());
+        show_date.setText(String.valueOf(date));
+    }
+
+    private void LocalTime(){
+        LocalTime currentTime=LocalTime.now();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        show_time.setText(currentTime.format(timeFormatter));
+    }
+
+    private void setGreetings() {
+        LocalTime currentTime = LocalTime.now();
+        String greeting = (currentTime.getHour() < 12) ? "Good Morning !!!" : "Good Evening !!!";
+        time.setText(greeting);
     }
 
     private void lastLoginID() {
@@ -111,6 +140,21 @@ public class DashBoardController {
             e.printStackTrace();
         }
     }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/DashBoard.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) btnStudentRegister.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.centerOnScreen();
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @FXML
     void btnUsersOnAction(ActionEvent event) {
